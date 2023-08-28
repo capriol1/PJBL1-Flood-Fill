9bf0ae11d2ff315903b26cdab2c62be74fa07f01
Breno Sedrez <breno.sedrez@gmail.com>
2023-08-27 21:48:17 -0300

    feat: add FloodFill class
    - criado a classe FloodFill

--- src/FloodFill.java
+++ src/FloodFill.java
@@ -1,6 +1,60 @@
-public class FloodFill {
-    public void Fill(Matriz matriz, int valor1, int valor0){
-        
+import java.util.Arrays;
 
+public class FloodFill {
+    private static final int BACKGROUND_COLOR = 1;
+    private static final int NEW_COLOR = 2;
+
+    public static void floodFill(int[][] image, int startX, int startY) {
+        int rows = image.length;
+        int cols = image[0].length;
+        int targetColor = image[startX][startY];
+
+        if (targetColor == NEW_COLOR) {
+            return;
+        }
+
+        Queue<Integer> queue = new Queue<>(rows * cols);
+        queue.enqueue(startX * cols + startY);
+
+        while (!queue.isEmpty()) {
+            int pixel = queue.dequeue();
+            int x = pixel / cols;
+            int y = pixel % cols;
+            (row < 0 || row >= numRows || col < 0 || col >= numCols || image.getPixel(row, col) != oldValue)
+            if (x < 0 || y < 0 || x >= rows  || y >= cols || image[x][y] != targetColor) {
+                continue;
+            }
+
+            image[x][y] = NEW_COLOR;
+
+            queue.enqueue((x - 1) * cols + y); // Up
+            queue.enqueue((x + 1) * cols + y); // Down
+            queue.enqueue(x * cols + (y - 1)); // Left
+            queue.enqueue(x * cols + (y + 1)); // Right
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] image = {
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+                {1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+                {1, 1, 1, 1, 1, 1, 1, 0, 1, 1},
+                {1, 1, 1, 1, 1, 1, 0, 1, 1, 1},
+                {1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+                {1, 1, 1, 1, 0, 1, 1, 1, 1, 1},
+                {1, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+                {1, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+                {1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+                {0, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+        };
+
+        int startX = 1;
+        int startY = 2;
+
+        floodFill(image, startX, startY);
+
+        for (int[] row : image) {
+            System.out.println(Arrays.toString(row));
+        }
     }
 }
--- src/Matriz.java
+++ src/Matriz.java
@@ -1,6 +1,7 @@
 public class Matriz {
-    private int [][] matriz;
-    public Matriz(){
+    private int[][] matriz;
+
+    public Matriz() {
         this.matriz = new int[][]{
                 {1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
                 {1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
